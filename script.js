420ed3f98d6861085edd84e6663e308a7a72d956
Jesper Huovila <jesperhuovila@hotmail.se>
2024-07-06 10:48:38 +0200

    started on the js part, DOMs and functions

--- script.js
+++ script.js
@@ -1,9 +1,75 @@
-const playerScore = 0;
-const computerScore = 0;
-const playerScoreSpan = document.getElementById("player-score");
-const computerScoreSpan = document.getElementById("computer-score");
-const playersScoreDiv = document.getQuarySelector(".players-score");
-const resultDiv = document.getQuarySelector(".result");
+let playerScore = 0;
+let computerScore = 0;
+let playerScoreSpan = document.getElementById("player-score");
+let computerScoreSpan = document.getElementById("computer-score");
+const playersScoreDiv = document.querySelector(".players-score");
+const resultDiv = document.querySelector(".result");
 const rockDiv = document.getElementById("rock");
 const paperDiv = document.getElementById("paper");
 const scissorsDiv = document.getElementById("scissors");
+
+
+// randomnumber return as choices(rock,paper..)
+function getComputerChoice() { 
+  const choices = ["rock", "paper", "scissors"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices [randomNumber];
+}
+
+function win() {
+  playerScore++;
+  playerScoreSpan.innerText = playerScore;
+}
+
+function lose() {
+  computerScore++;
+  computerScoreSpan.innerText = computerScore;
+}
+
+function draw() {
+  
+}
+
+// player choice vs computer choice 
+function play(playerChoice) {
+  const computerChoice = getComputerChoice();
+  if(playerChoice === "rock" && computerChoice === "scissors") {
+    win();
+  } else if(playerChoice === "rock" && computerChoice === "paper") {
+    lose();
+  } else if(playerChoice === "rock" && computerChoice === "rock") {
+    draw();
+  }
+
+  if(playerChoice === "paper" && computerChoice === "rock") {
+    win();
+  } else if(playerChoice === "paper" && computerChoice === "scissors") {
+    lose();
+  } else if(playerChoice === "paper" && computerChoice === "paper") {
+    draw();
+  }
+
+  if(playerChoice === "scissors" && computerChoice === "paper") {
+    win();
+  } else if(playerChoice === "scissors" && computerChoice === "rock") {
+    lose();
+  } else if(playerChoice === "scissors" && computerChoice === "scissors") {
+    draw();
+  }
+}
+
+function main() {
+  rockDiv.addEventListener('click', function() {
+    play("rock");
+  })
+
+  paperDiv.addEventListener('click', function() {
+    play("paper");
+  })
+
+  scissorsDiv.addEventListener('click', function() {
+    play("scissors");
+  })
+}
+
+main();
